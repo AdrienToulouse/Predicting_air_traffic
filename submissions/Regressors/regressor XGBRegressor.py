edc0a7e82f73b4e386353001a92040de3614565d
Adrien Toulouse <adrien.toulouse10@gmail.com>
2019-11-18 17:40:34 +0100

    regressor

--- submissions/Regressors/regressor XGBRegressor.py
+++ submissions/Regressors/regressor XGBRegressor.py
@@ -7,9 +7,8 @@ class Regressor(BaseEstimator):
         self.reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
        colsample_bytree=1, gamma=0, learning_rate=0.1, max_delta_step=0,
        max_depth=3, min_child_weight=1, missing=None, n_estimators=100,
-       n_jobs=1, nthread=None, objective='reg:linear', random_state=0,
-       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
-       silent=True, subsample=1)
+       n_jobs=1, objective='reg:linear', random_state=0,
+       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, subsample=1)
 
     def fit(self, X, y):
         self.reg.fit(X, y)
