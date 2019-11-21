885b3588322b9fadaf1eb379166763a23f26983c
paulantoinegirard <paul-antoine.girard@polytechnique.edu>
2019-11-21 16:33:07 +0100

    ex

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -17,13 +17,14 @@ class FeatureExtractor(object):
 
         path = os.path.dirname(__file__)
 
-# import data_weather and merge it
+# import external_data and merge it
 
         ext_data = pd.read_csv(os.path.join(path, 'external_data.csv'))
 
-        external_data = ext_data[['DateOfDeparture', 'Departure','Arrival', 'Mean TemperatureC', 'Distance',
-                      'MeanDew PointC', 'Mean Humidity', 'Min VisibilitykM', 'Mean Sea Level PressurehPa','CloudCover',
-                      'Max Wind SpeedKm/h', 'Precipitationmm', 'Events','dep_encod','ar_encod','Revenue','Number_hab']]
+        external_data = ext_data[['DateOfDeparture', 'Departure', 'Arrival', 'Mean TemperatureC', 'Distance',
+                      'MeanDew PointC', 'Mean Humidity', 'Mean VisibilityKm', 'Mean Sea Level PressurehPa', 'CloudCover',
+                      'Max Wind SpeedKm/h', 'Precipitationmm', 'Events', 'dep_encod', 'ar_encod', 'Revenue', 'Number_hab',
+                      'Oil_price']]
 
         X_encoded = pd.merge(X_encoded, external_data, how='left',
             left_on=['DateOfDeparture', 'Departure', 'Arrival'],
@@ -38,27 +39,27 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.drop(['Arrival','Departure'], axis=1)
 
 #Mean encoding
-        #X_encoded.loc[:, 'd_ATL': 'd_SFO'] = X_encoded.loc[:, 'd_ATL': 'd_SFO'].mul(X_encoded.loc[:, 'dep_encod'], axis='rows')
-        #X_encoded.loc[:, 'a_ATL': 'a_SFO'] = X_encoded.loc[:, 'a_ATL': 'a_SFO'].mul(X_encoded.loc[:, 'ar_encod'], axis='rows')
+        X_encoded.loc[:, 'd_ATL': 'd_SFO'] = X_encoded.loc[:, 'd_ATL': 'd_SFO'].mul(X_encoded.loc[:, 'dep_encod'], axis='rows')
+        X_encoded.loc[:, 'a_ATL': 'a_SFO'] = X_encoded.loc[:, 'a_ATL': 'a_SFO'].mul(X_encoded.loc[:, 'ar_encod'], axis='rows')
 
-        #X_encoded.drop(['dep_encod', 'ar_encod'], axis=1, inplace=True)
+        X_encoded.drop(['dep_encod', 'ar_encod'], axis=1, inplace=True)
 
 #One hot encoder for dates
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
-        X_encoded['month'] = X_encoded['DateOfDeparture'].dt.month
+        #X_encoded['month'] = X_encoded['DateOfDeparture'].dt.month
         X_encoded['day'] = X_encoded['DateOfDeparture'].dt.day
         X_encoded['weekday'] = X_encoded['DateOfDeparture'].dt.weekday
         X_encoded['week'] = X_encoded['DateOfDeparture'].dt.week
         X_encoded['n_days'] = X_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
 
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['year'], prefix='y'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
+        #X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['day'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
 
-        X_encoded.drop(['DateOfDeparture', 'year', 'month', 'day', 'weekday', 'week', 'n_days'], axis=1, inplace=True)
+        X_encoded.drop(['DateOfDeparture', 'year', 'day', 'weekday', 'week', 'n_days'], axis=1, inplace=True)
 
 #reservations
         #X_encoded.loc[:, 'WeeksToDeparture' : 'std_wtd'] = StandardScaler().fit_transform(X_encoded.loc[:, 'WeeksToDeparture' : 'std_wtd'])
@@ -78,8 +79,8 @@ class FeatureExtractor(object):
         X_encoded['Precipitationmm'] = X_encoded['Precipitationmm'].fillna(X_encoded['Precipitationmm'].mean())
 
 #create columns
-        X_encoded['Precipitationmm*CloudCover'] = X_encoded['Precipitationmm'] * X_encoded['CloudCover']
-        X_encoded['Temperature*Humidity'] = X_encoded['Mean TemperatureC'] * X_encoded['Mean Humidity']
+        #X_encoded['Precipitationmm*CloudCover'] = X_encoded['Precipitationmm'] * X_encoded['CloudCover']
+        #X_encoded['Temperature*Humidity'] = X_encoded['Mean TemperatureC'] * X_encoded['Mean Humidity']
 
 # Regroup values
         X_encoded['Weeks_to_dep_int'] = pd.qcut(X_encoded['WeeksToDeparture'], 4)
@@ -94,19 +95,19 @@ class FeatureExtractor(object):
         #X_encoded['weekend'] = X_encoded['wd_5'] + X_encoded['wd_6']
         #X_encoded.drop(['wd_0', 'wd_1', 'wd_2', 'wd_3', 'wd_4', 'wd_5','wd_6'], axis=1, inplace=True)
 # StandardScaler
-        X_encoded.loc[:, 'Precipitationmm*CloudCover' : 'Temperature*Humidity'] = StandardScaler().fit_transform(
-                                                            X_encoded.loc[:, 'Precipitationmm*CloudCover' : 'Temperature*Humidity'])
+        #X_encoded.loc[:, 'Precipitationmm*CloudCover' : 'Temperature*Humidity'] = StandardScaler().fit_transform(
+                                                            #X_encoded.loc[:, 'Precipitationmm*CloudCover' : 'Temperature*Humidity'])
 
-        X_encoded.loc[:, 'Distance' : 'Max Wind SpeedKm/h'] = StandardScaler().fit_transform(
-                                                            X_encoded.loc[:, 'Distance' : 'Max Wind SpeedKm/h'])
+        #X_encoded.loc[:, 'MeanDew PointC' : 'Max Wind SpeedKm/h'] = StandardScaler().fit_transform(
+                                                            #X_encoded.loc[:, 'MeanDew PointC' : 'Max Wind SpeedKm/h'])
 
-        X_encoded.loc[:, 'dep_encod' : 'Revenue'] = StandardScaler().fit_transform(
-                                                            X_encoded.loc[:, 'dep_encod' : 'Revenue'])
+        #X_encoded.loc[:, 'Revenue' : 'Oil_price'] = StandardScaler().fit_transform(
+                                                            #X_encoded.loc[:, 'Revenue' : 'Oil_price'])
 #drop what we don't use for now
-        X_encoded.drop(['Mean TemperatureC', 'Mean Humidity', 'Precipitationmm', 'CloudCover'], axis=1, inplace=True)
-        X_encoded.drop(['MeanDew PointC', 'Mean Sea Level PressurehPa'], axis=1, inplace=True)
+        #X_encoded.drop(['Mean TemperatureC', 'Mean Humidity', 'Precipitationmm', 'CloudCover'], axis=1, inplace=True)
+        #X_encoded.drop(['MeanDew PointC', 'Mean Sea Level PressurehPa'], axis=1, inplace=True)
         #X_encoded.drop(['dep_encod','ar_encod','Revenue'], axis=1, inplace=True)
-        X_encoded.drop(['ar_encod'], axis=1, inplace=True)
+
 #end
 
         X_array = X_encoded.values
--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -1,13 +1,15 @@
 import xgboost as xgb
 from sklearn.base import BaseEstimator
 
-#reg_alpha=0.01, reg_lambda=0,
+
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.reg = xgb.XGBRegressor(colsample_bylevel=0.8,
-       colsample_bytree=0.55, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=7, min_child_weight=4, n_estimators=300,
-       n_jobs=1, reg_lambda=0.001, subsample=0.95, objective='reg:squarederror')
+        self.reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+       colsample_bytree=0.7, gamma=0, learning_rate=0.3, max_delta_step=0,
+       max_depth=7, min_child_weight=5, missing=None, n_estimators=1000,
+       n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
+       reg_alpha=0.001, reg_lambda=0, seed=None,
+       silent=True, subsample=0.8)
 
     def fit(self, X, y):
         self.reg.fit(X, y)
