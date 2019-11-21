0651c551bb2a7ec37197b531eaf115244b5f78fb
Adrien Toulouse <adrien.toulouse10@gmail.com>
2019-11-21 14:29:11 +0100

    regroup variables

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -1,12 +1,11 @@
 import xgboost as xgb
 from sklearn.base import BaseEstimator
 
-#reg_alpha=0.01, reg_lambda=0,
 class Regressor(BaseEstimator):
     def __init__(self):
         self.reg = xgb.XGBRegressor(colsample_bylevel=0.8,
-       colsample_bytree=0.55, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=7, min_child_weight=4, n_estimators=300,
+       colsample_bytree=0.55, gamma=0, learning_rate=0.3, max_delta_step=0,
+       max_depth=7, min_child_weight=4, n_estimators=1000,
        n_jobs=1, reg_lambda=0.001, subsample=0.95, objective='reg:squarederror')
 
     def fit(self, X, y):
