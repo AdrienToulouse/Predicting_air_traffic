80336cdc5088127da46847e0fb8a82f794c74300
Adrien Toulouse <adrien.toulouse10@gmail.com>
2019-11-27 15:51:12 +0100

    rapport

--- submissions/Regressors/regressor XGBRegressor moins overfit.py
+++ submissions/Regressors/regressor XGBRegressor moins overfit.py
@@ -30,3 +30,23 @@ class Regressor(BaseEstimator):
 
     def predict(self, X):
         return self.reg.predict(X)
+
+
+        import xgboost as xgb
+        from sklearn.base import BaseEstimator
+
+
+        class Regressor(BaseEstimator):
+            def __init__(self):
+                self.reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+               colsample_bytree=0.7, gamma=0, learning_rate=0.3, max_delta_step=0,
+               max_depth=7, min_child_weight=5, missing=None, n_estimators=500,
+               n_jobs=1, nthread=1, objective='reg:linear', random_state=0,
+               reg_alpha=0.001, reg_lambda=0, seed=None,
+               silent=True, subsample=0.8)
+
+            def fit(self, X, y):
+                self.reg.fit(X, y)
+
+            def predict(self, X):
+                return self.reg.predict(X)
