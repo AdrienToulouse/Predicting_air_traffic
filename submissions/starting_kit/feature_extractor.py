2c8aa380bc196bd4cd810e9f73ae311e32da617a
paulantoinegirard <paul-antoine.girard@polytechnique.edu>
2019-11-29 13:28:31 +0100

    0.2511111

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import os
 import numpy as np
@@ -24,7 +23,10 @@ class FeatureExtractor(object):
         path = os.path.dirname(__file__)
 
 # import data_weather and merge it
-        external_data = pd.read_csv(os.path.join(path, 'external_data.csv'))
+        ext_data = pd.read_csv(os.path.join(path, 'external_data.csv'))
+
+        external_data = ext_data[['DateOfDeparture', 'Departure', 'Arrival', 'Distance',
+                      'dep_encod', 'ar_encod', 'Events', 'Revenue']]
 
         X_encoded = pd.merge(X_encoded, external_data, how='left',
             left_on=['DateOfDeparture', 'Departure', 'Arrival'],
@@ -37,7 +39,7 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
         X_encoded.drop(['Arrival','Departure'], axis=1, inplace=True)
 
-#Mean encoding
+#Mean encoding airport
         X_encoded.loc[:, 'ATL': 'SFO'] = X_encoded.loc[:, 'ATL': 'SFO'].mul(X_encoded.loc[:, 'dep_encod'], axis='rows')
         arr = X_encoded.loc[:, 'a_ATL': 'a_SFO'].mul(- 1 * X_encoded.loc[:, 'ar_encod'], axis='rows')
 
@@ -47,7 +49,7 @@ class FeatureExtractor(object):
         X_encoded.drop(['dep_encod', 'ar_encod'], axis=1, inplace=True)
         X_encoded.drop(X_encoded.loc[:, 'a_ATL': 'a_SFO'].columns, axis=1, inplace=True)
 
-#One hot encoder for dates
+#Encoding date
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
         X_encoded['month'] = X_encoded['DateOfDeparture'].dt.month
@@ -63,11 +65,24 @@ class FeatureExtractor(object):
 
         X_encoded.drop(['DateOfDeparture', 'year', 'month', 'day', 'weekday', 'week', 'std_wtd'], axis=1, inplace=True)
 
+#meteo
+
+        X_encoded['Events'].fillna(0, inplace=True)
+        X_encoded['Events'] = X_encoded['Events'].replace(['Rain','Fog'], 1)
+        X_encoded['Events'] = X_encoded['Events'].replace(['Rain-Thunderstorm','Fog-Rain-Thunderstorm', \
+                                                'Rain-Snow','Snow','Fog-Rain','Thunderstorm','Fog-Snow', \
+                                                'Fog-Rain-Snow','Fog-Rain-Snow-Thunderstorm', \
+                                                'Rain-Snow-Thunderstorm','Rain-Hail-Thunderstorm', \
+                                                'Fog-Rain-Hail-Thunderstorm', \
+                                                'Rain-Thunderstorm-Tornado'], 2)
+
+ #reservations mean
         X_encoded.loc[X_encoded['WeeksToDeparture'] <= 9.524, 'WeeksToDeparture'] = 0
         X_encoded.loc[(X_encoded['WeeksToDeparture'] > 9.524) & (X_encoded['WeeksToDeparture'] <= 11.3), 'WeeksToDeparture'] = 1
         X_encoded.loc[(X_encoded['WeeksToDeparture'] > 11.3) & (X_encoded['WeeksToDeparture'] <= 13.24), 'WeeksToDeparture'] = 2
         X_encoded.loc[ X_encoded['WeeksToDeparture'] > 13.24, 'WeeksToDeparture'] = 3
         X_encoded['WeeksToDeparture'] = X_encoded['WeeksToDeparture'].astype(int)
+
 
         X_array = X_encoded.values
         return X_array
