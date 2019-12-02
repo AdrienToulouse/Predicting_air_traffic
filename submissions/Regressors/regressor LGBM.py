bfe0423baf13d86dcac48e9885879fad298d1e8d
paulantoinegirard <paul-antoine.girard@polytechnique.edu>
2019-12-02 22:57:08 +0100

    new model

--- submissions/Regressors/regressor LGBM.py
+++ submissions/Regressors/regressor LGBM.py
@@ -4,10 +4,15 @@ from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.reg = LGBMRegressor(num_leaves=100,
-                   learning_rate=0.01,
-                   n_estimators=1000,
-                   max_depth=-1)
+        self.reg = LGBMRegressor(num_leaves=40,
+                    boosting_type='gbdt',
+                    objective='regression',
+                    learning_rate=0.1,
+                    max_depth=-1,
+                    n_estimators=1800,
+                    max_bin=255, silent=True,
+                    reg_alpha=.001,
+                    reg_lambda=.01)
 
     def fit(self, X, y):
         self.reg.fit(X, y)
